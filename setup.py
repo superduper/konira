0d4f6fe53205a49e93c7e90a4789452c5436dbef
Victor Miroshnikov <me@vmiroshnikov.com>
2013-10-11 00:36:25 +0200

    bump version

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ testing look good and easy.
 
 setup(
     name             = "konira",
-    version          = "0.3.2",
+    version          = "0.3.3",
     packages         = ['konira', 'konira.ext'],
     scripts          = scripts,
     author           = "Alfredo Deza",
